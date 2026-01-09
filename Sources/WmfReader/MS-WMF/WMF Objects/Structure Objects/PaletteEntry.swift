d0ecf11e1b524760b96233101839a5c5b148b96a
Hugh Bellamy <hughbellars@gmail.com>
2026-01-09 14:02:31 +0000

    Update PaletteEntry.swift
    Fixes #4

--- Sources/WmfReader/MS-WMF/WMF Objects/Structure Objects/PaletteEntry.swift
+++ Sources/WmfReader/MS-WMF/WMF Objects/Structure Objects/PaletteEntry.swift
@@ -10,10 +10,10 @@ import DataStream
 /// [MS-WMF] 2.2.2.13 PaletteEntry Object
 /// The PaletteEntry Object defines the color and usage of an entry in a palette
 public struct PaletteEntry {
-    public let values: PaletteEntryFlag
-    public let blue: UInt8
-    public let green: UInt8
     public let red: UInt8
+    public let green: UInt8
+    public let blue: UInt8
+    public let values: PaletteEntryFlag
     
     public init(dataStream: inout DataStream) throws {
         /// Red (1 byte): An 8-bit unsigned integer that defines the red intensity value for the palette entry
